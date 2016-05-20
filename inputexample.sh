3b924501995d805b5c327262339e8b9e8a712930
mona <jalal@wisc.edu>
2016-05-20 03:43:29 -0500

    input && select statement

---
+++ inputexample.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "what is your fav animal?"
+read animal
+echo what is your password?
+read -s password
+echo what is your name?
+read -p name
+
+#select statement
+select choice in "cat" "bird" "ananas" "pineapple"
+do
+  echo you selected $choice
+  #you should use break or if it will ask for your choice like forever
+  break
+done
+
+#select with case
+select animal in "cat" "dog" "bird" "snake"
+do
+  case $animal in
+  cat) echo cats meow; break ;;
+  dog) echo dogs haap haap; break ;;
+  bird) echo birds jik jik; break ;;
+  snake) echo snakes are dangerous; break ;;
+  *) echo I don\'t understand your selection; break;;
+esac
+done
+
