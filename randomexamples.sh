0e188cc5e3a9e81066ea4c0070ce99236d130ef5
mona <jalal@wisc.edu>
2016-05-23 11:00:36 -0500

    random bash

--- randomexamples.sh
+++ randomexamples.sh
@@ -9,3 +9,11 @@ $( echo  "1 2 3 4 5 6" | xargs -t -n3 )
 allfiles=$( ls | xargs cat | less )
 numwords=$( echo "$allfiles" | wc -w )
 $( cat fruits.txt | sort | uniq | xargs -I {} mkdir -p  {} )
+
+#print the PID of processes that use more than 3% of CPU time
+ps -eo pri,pid,user,nice,pcpu,comm  | awk '{if($5 >= 3) print $2}'
+
+#detect which process is using port 8080
+sudo lsof -i:8080
+#find the PID and kill it (unfortunately sometimes you should kill it nasty way with -9)
+#sudo kill -9 PID
