65d23ea4a7cd50dec38e5e40dcd0d355e5b8b2c0
mona <jalal@wisc.edu>
2016-05-23 21:56:36 -0500

    triangle fixed

--- triangle.sh
+++ triangle.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-
+#author: Mona Jalal
 read x y z
-
-if [[ x -eq z ]] && [[ x -eq y ]] && [[ y -eq z ]]; then
-  echo EQUILATERAL
-elif [[ x -eq z ]] && [[ x -eq y ]]; then
-  echo ISOSCELES
-elif [[ y -eq z ]] && [[ y -eq x ]]; then
-  echo ISOSCELES
-elif [[ z -eq x ]] && [[ z -eq y ]]; then
-  echo ISOSCELES
-else
+#for i in x y z
+#do
+#  (( i >= 1 )) && (( i <= 1000 )) || echo error
+#done
+xplusy=$x+$y
+yplusz=$y+$z
+zplusx=$z+$x
+if [[ $x -ge 1 ]] && [[ $x -le 1000 ]] &&
+   [[ $y -ge 1 ]] && [[ $y -le 1000 ]] &&
+   [[ $z -ge 1 ]] && [[ $z -le 1000 ]]; then
+if [[ $xplusy -gt $z ]] && [[ $yplusz -gt $x ]] && [[ $zplusx -gt $y ]]; then
+if [[ $x -ne $z ]] && [[ $x -ne $y ]] && [[ $y -ne $z ]]; then
   echo SCALENE
+elif [[ $x -eq $z ]] && [[ $x -eq $y ]] && [[ $y -eq $z ]]; then
+  echo EQUILATERAL
+else
+  echo ISOSCELES
+fi
+fi
 fi
